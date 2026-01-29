7c520168ca5b2e8a77a075b05c23b56dd334ebcf
22401673 <peronidylan06@gmail.com>
2026-01-29 17:53:02 +0100

    Commit.

--- src/archilog/__init__.py
+++ src/archilog/__init__.py
@@ -1,6 +1,7 @@
 import uuid
 import click
 import sqlite3
+from datetime import date
 from dataclasses import dataclass
 
 
@@ -27,19 +28,20 @@ def delete_cagnotte(name: str):
     click.echo(f"Cagnotte deleted : '{name}'")
 
 @cli.command()
-@click.option("--name", prompt="Cagnotte name", help="The name of the cagnotte.")
+@click.option("--cagnotte_name", prompt="Cagnotte name", help="The name of the cagnotte.")
 @click.option("--participant_name", prompt="Participant name", help="The name of the participant.")
 @click.option("--amount", prompt="Amount of the expense", help="The amount of the expense.")
-def add_expense(cagnotte_name: str, participant_name: str, amount: float):
+@click.option("--expense_date", prompt="Date of the expense", help="The date of the expense.")
+def add_expense(cagnotte_name: str, participant_name: str, amount: float, expense_date: date):
     click.echo(f"Cagnotte    : '{cagnotte_name}'")
     click.echo(f"Participant : '{participant_name}'")
+    click.echo(f"Date        : '{expense_date}'")
     click.echo(f"Amount      : '{amount}' $")
 
 @cli.command()
-@click.option("--name", prompt="Cagnotte name", help="The name of the cagnotte.")
+@click.option("--cagnotte_name", prompt="Cagnotte name", help="The name of the cagnotte.")
 @click.option("--participant_name", prompt="Participant name", help="The name of the participant.")
 def delete_expense(cagnotte_name: str, participant_name: str):
     click.echo(f"'{participant_name}' has retired his expense in the cagnotte '{cagnotte_name}'.")
 
 def calculate():
-    pass
