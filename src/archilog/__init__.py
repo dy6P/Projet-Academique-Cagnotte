db7d5ea2bd8d47b929edaa0c00cf8fd3e571abe2
22401673 <peronidylan06@gmail.com>
2026-01-29 17:30:33 +0100

    Commit.

--- src/archilog/__init__.py
+++ src/archilog/__init__.py
@@ -1,8 +1,6 @@
-from datetime import date
-from datetime import time
-import click
 import uuid
-
+import click
+import sqlite3
 from dataclasses import dataclass
 
 
@@ -11,25 +9,37 @@ class Item:
     id: uuid.UUID
     name: str
 
-
 @click.group()
 def cli():
     pass
 
+#sqlite3.connect("../")
+
 
 @cli.command()
-@click.option("-n", "--name", prompt="Name", help="The name of the cagnotte.")
-
-
+@click.option("--name", prompt="Cagnotte to be created", help="The name of the cagnotte.")
 def create_cagnotte(name: str):
-    item = Item(uuid.uuid4(), name)
-    click.echo(item)
+    click.echo(f"Cagnotte created : '{name}'")
 
+@cli.command()
+@click.option("--name", prompt="Cagnotte to be removed", help="The name of the cagnotte.")
 def delete_cagnotte(name: str):
-    pass
+    click.echo(f"Cagnotte deleted : '{name}'")
 
-def add_depense(cagnotte_name: str, participant_name: str, amount: float, date: date, time: time):
-    pass
+@cli.command()
+@click.option("--name", prompt="Cagnotte name", help="The name of the cagnotte.")
+@click.option("--participant_name", prompt="Participant name", help="The name of the participant.")
+@click.option("--amount", prompt="Amount of the expense", help="The amount of the expense.")
+def add_expense(cagnotte_name: str, participant_name: str, amount: float):
+    click.echo(f"Cagnotte    : '{cagnotte_name}'")
+    click.echo(f"Participant : '{participant_name}'")
+    click.echo(f"Amount      : '{amount}' $")
 
-def delete_depense(cagnotte_name: str, participant_name: str, date: date, time: time):
+@cli.command()
+@click.option("--name", prompt="Cagnotte name", help="The name of the cagnotte.")
+@click.option("--participant_name", prompt="Participant name", help="The name of the participant.")
+def delete_expense(cagnotte_name: str, participant_name: str):
+    click.echo(f"'{participant_name}' has retired his expense in the cagnotte '{cagnotte_name}'.")
+
+def calculate():
     pass
